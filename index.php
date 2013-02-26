73877b3dc0cef11385a31ac02b323458e0500643
Gabriel Izaias <gabriel.izaias@gmail.com>
2013-02-26 09:32:41 -0300

    Adiciona index do sistema
    Este arquivo vai rotear todas as requisições ao sistema

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+* Alf CMS
+*
+* PHP 5
+*
+* Alf CMS
+* Copyright 2013-2013, Tonight Systems, Inc. (https://github.com/tonightsystems)
+*
+* Licensed under The MIT License
+* Redistributions of files must retain the above copyright notice.
+*
+* @package      Alf
+* @copyright    Copyright 2013-2013, Tonight Systems, Inc.
+* @link         https://github.com/tonightsystems/alf-cms
+* @license      MIT License (http://www.opensource.org/licenses/mit-license.php)
+*/
+
+/**
+ * Define as constantes principais do sistema
+ */
+define('ROOT', dirname(__FILE__));
+define('DS', DIRECTORY_SEPARATOR);
+define('CORE', ROOT . DS . 'core');
+define('THEMES', ROOT . DS . 'themes');
