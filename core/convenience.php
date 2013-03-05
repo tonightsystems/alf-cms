81e9490d549994e3a6e929bc56fe57776c7f443c
Gabriel Izaias <gabriel.izaias@gmail.com>
2013-03-04 21:00:46 -0300

    Adiciona constantes de tempo

--- core/convenience.php
+++ core/convenience.php
@@ -16,6 +16,14 @@
 * @license      MIT License (http://www.opensource.org/licenses/mit-license.php)
 */
 
+define('SECOND', 1);
+define('MINUTE', 60);
+define('HOUR', 3600);
+define('DAY', 86400);
+define('WEEK', 604800);
+define('MONTH', 2592000);
+define('YEAR', 31536000);
+
 /**
  * Imprime tags `<pre>` ao redor do resultado de uma chamada à função `print_r()`
  *
