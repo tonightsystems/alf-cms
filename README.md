1a4bea04e77de91c30bf7993258c75543e66e459
Rômulo Barbosa <barbosaromulo@gmail.com>
2013-05-22 23:40:34 -0300

    Adiciona arquivos do Starter

---
+++ README.md
@@ -0,0 +1,22 @@
+# Starter: Quick start new web projects
+
+Starter is a compilation of snippets and best practices for web development, found in different resources such as Bootstrap, H5BP and several others.
+
+## Conventions
+
+* Indentation is 4 spaces;
+* Readability over compression;
+* Browser support
+    * Chrome;
+    * Firefox (FF3.5+);
+    * Opera (Latest);
+    * Safari;
+    * Internet Explorer (IE8+);
+
+
+## License
+
+Copyright (c) 2012 [Gabriel Izaias](gabrielizaias.com)<br />
+Dual licensed under the MIT and GPL licenses:<br />
+http://www.opensource.org/licenses/mit-license.php<br />
+http://www.gnu.org/licenses/gpl.html
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8">
+<!--[if IE]><![endif]-->
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title></title>
+<meta name="author" content="" />
+<link rel="shortcut icon" href="" />
+<!-- Touch Icons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="apple-touch-icon-144x144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-icon-114x114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-72x72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png">
+<!-- Facebook -->
+<meta property="og:title" content=""/>
+<meta property="og:type" content="website"/>
+<meta property="og:url" content=""/>
+<meta property="og:image" content=""/>
+<meta property="og:site_name" content=""/>
+<meta property="og:description" content=""/>
+<!-- HTML5 FOR IE -->
+<!--[if lt IE 9]><script src="js/lib/html5.js"></script><![endif]-->
+<!-- STYLESHEETS -->
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div id="all">
+    <header id="header"></header>
+    <div id="main"></div>
+    <footer id="footer"></footer>
+</div>
+<!-- SCRIPTS -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="js/script.js"></script>
+<!-- Google Analytics -->
+<!--<script>
+var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>-->
+</body>
+</html>
