106cabc48ebd702c2cacbea1e40fef7f10c217fc
Rômulo Barbosa <barbosaromulo@gmail.com>
2013-05-23 22:36:12 -0300

    Adiciona o box do Facebook

--- index.php
+++ index.php
@@ -54,6 +54,11 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, aut, error, dolorum libero odio accusantium aspernatur commodi perferendis sunt iusto assumenda sapiente dignissimos ex. Ipsam, veritatis reprehenderit distinctio nemo necessitatibus.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, aut, error, dolorum libero odio accusantium aspernatur commodi perferendis sunt iusto assumenda sapiente dignissimos ex. Ipsam, veritatis reprehenderit distinctio nemo necessitatibus.</p>
             </div>
+            <div id="sidebar">
+                <div id="facebook">
+                    <div class="fb-like" data-href="https://www.facebook.com/MarcoAurelioNoFace" data-send="false" data-width="250" data-show-faces="true"></div>
+                </div>
+            </div>
         </div>
     </div>
     <footer id="footer"></footer>
@@ -61,6 +66,15 @@
 <!-- SCRIPTS -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="js/script.js"></script>
+<!-- facebook -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <!-- Google Analytics -->
 <!--<script>
 var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
