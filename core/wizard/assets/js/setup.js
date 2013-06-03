be21cd7bb3ac4f24e1cad06d5d4fda9a9355caa8
Gabriel Izaias <gabriel.izaias@gmail.com>
2013-06-02 21:20:18 -0300

    Remove script das abas do setup
    Não é mais necessário já que agora a instalação é separada em passos

--- core/wizard/assets/js/setup.js
+++ core/wizard/assets/js/setup.js
@@ -1,18 +1,4 @@
 $(function(){
-    $('#btn-setup').on('click', function(e){
-        e.preventDefault();
-        $('.hero-unit:first').addClass('none');
-
-        $('#steps')
-            .removeClass('none')
-            .find('#tabs a:first').tab('show');
-    });
-
-    $('#tabs a').on('click', function(e) {
-      e.preventDefault();
-      $(this).tab('show');
-    });
-
     $('#themes input:radio').on('change', function(e){
         var span = $(this).parent().find('span');
         $('#themes').find('span.active').not(span).removeClass('active');
